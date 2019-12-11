2c09c651d5180de389d0298fb8fc1cb201d23990
Your Name <cetin>
2019-12-11 12:24:45 +0300

    cesitli ozellikler eklendi

--- 17011603.c
+++ 17011603.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -6,23 +5,19 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define STDIN 0
 #define STDOUT 1
 
-/* TODOs */
-//pipe buffer problem
-//rm exec?
-//shell exit
-
 
 /* Fonksiyon Prototipleri */
-void execute_command();
-void parse_command_line(char* command_line);
-void set_pipe_attributes(int fd[2]);
-void change_std_descriptors(int fd[2], int* original_stdin, int* original_stdout);
-void restore_std_descriptor(int original_stddesc, int pipe_flag, int stddesc);
-void init_pipe_components(int* original_stdin, int* original_stdout);
+void execute_command(char**);
+void parse_command_line(char*);
+void restore_std_descriptor(int, int, int);
+void connect_std_to_pipe(int, int);
+char** prepare_next_commmand(char**);
+void ignore_int_signal();
 
 
 /* Fonksiyon Tanimlari */
@@ -37,6 +32,10 @@ void restore_std_descriptor(int original_stddesc, int pipe_flag, int stddesc) {
         dup2(original_stddesc, stddesc);
         close(original_stddesc);
     }
+    /* Pipe kullanilmamis ise, std betimleyicisini restore etmek yerine, yalnizca ekstra olarak acilan std betimleyicisi kapatilir */
+    else {
+        close(original_stddesc);
+    }
 
 }
 
@@ -46,9 +45,21 @@ void connect_std_to_pipe(int pipe_fd, int std_choice) {
     close(pipe_fd);
 }
 
+/* Pipe operatoru kullanildigi durumda, bir sonraki komut icin gerekli alanlar ayrilir, ilklendirmeler yapilir */
+char** prepare_next_commmand(char** arguments) {
+
+    free(arguments);
+    arguments = (char**)calloc(50, sizeof(char*));
+
+    arguments[0] = strtok (NULL, " \n");
+    return arguments;
+
+}
+
 /* Bu fonksiyon, uygun formatte gelen argumanlar ile birlikte, belirtilen komutu calistirir */
 void execute_command(char** arguments) {
 
+    /* cd programi fork-exec ile dogru calismiyor. Bu yuzden manuel olarak chdir kullanildi */
     /* Tilde karakteri cd programi tarafindan taninmadigi icin, HOME path environment degiskeninden cekilmistir */
     if ( (arguments[0] != NULL) && (!strcmp(arguments[0],"cd")) ) {
         if (!strcmp(arguments[1],"~")) {
@@ -62,16 +73,26 @@ void execute_command(char** arguments) {
     else {
         /* Child Process */
         if (fork() == 0) {
-                /* Komut, tum parametreleriyle calistirilir */
-                execvp(arguments[0], arguments);
+            /* Komut, tum parametreleriyle calistirilir */
+            execvp(arguments[0], arguments);
+            if (arguments[0] != NULL) {
+                fprintf(stderr, "%s : boyle bir komut yok!\n",arguments[0] );
+                _Exit(1);
+            }
         }
-
         else {
-                /* Parent proses, child processde kosulan komutun tamamlanmasini bekliyor */
-                wait(NULL);
+            /* Parent proses, child processde kosulan komutun tamamlanmasini bekliyor */
+            wait(NULL);
         }
     }
 
+}
+
+/* Terminalde exit komutu girildiginde shell sonlandirilir */
+void check_exit(char* arg) {
+    if (arg != NULL && !strcmp(arg, "exit")) {
+        exit(0);
+    }
 }
 
 
@@ -98,7 +119,10 @@ void parse_command_line(char* command_line) {
     i = 0;
     pipe_flag = 0;
 
-    /* Mevcut stdin ve stdout referanslari saklaniyor */
+    /* Terminalden exit komutu girildiginde shell sonlandirilir */
+    check_exit(arg);
+
+    /* Mevcut stdin ve stdout referanslari duplicate ediliyor */
     original_stdin = dup(STDIN);
     original_stdout = dup(STDOUT);
 
@@ -108,16 +132,15 @@ void parse_command_line(char* command_line) {
         /* Eger pipe operatoru kullanilmissa mevcut komutun ciktisi bir pipe bufferina aktarilir (STDOUT yerine) */
         if (!strcmp(arg,"|")) {
 
-
             /* Yeni pipe olusturuluyor */
             pipe(fd);
 
-            /* stdout, pipe yazma ucuna baglaniyor */
+            /* stdout, pipe yazma ucuna baglaniyor (ayni zamanda dup2 vasitasiyla, bir onceki pipe yazma ucu kapatiliyor) */
             connect_std_to_pipe(fd[1], STDOUT);
 
             /* Eger onceden pipe operatoru ile karsilasildiysa */
             if (pipe_flag) {
-                /* stdin, Bir onceki komut icin olusturulan pipe in okuma ucuna baglaniyor. Boylece onceki komut ciktisi aliniyor */
+                /* stdin, bir onceki komut icin olusturulan pipe in okuma ucuna baglaniyor. Boylece onceki komut ciktisina ulasiliyor */
                 connect_std_to_pipe(prev_pipe_read_end, STDIN);
             }
 
@@ -131,12 +154,8 @@ void parse_command_line(char* command_line) {
             execute_command(arguments);
 
             /* Pipe operatorunden sonra gelen siradaki komut argumanlari icin gerekli alan ayrilir, degiskenler ilklendirilir */
-            free(arguments);
-            arguments = (char**)calloc(50, sizeof(char*));
-
+            arguments = prepare_next_commmand(arguments);
             i = 0;
-            arguments[i] = strtok (NULL, " \n");
-
         }
         else {
             arguments[i] = arg;
@@ -161,6 +180,7 @@ void parse_command_line(char* command_line) {
       connect_std_to_pipe(prev_pipe_read_end, STDIN);
     }
 
+
     /* Komut satirindaki son komut (pipe kullanilmissa) veya pipesiz tek komut calistirilir */
     execute_command(arguments);
 
@@ -168,11 +188,25 @@ void parse_command_line(char* command_line) {
     restore_std_descriptor(original_stdin, pipe_flag, STDIN);
 }
 
+/* SIGINT sinyalini default handleri degistirerek SIGINT yapilir */
+void ignore_int_signal() {
+    struct sigaction sa;
+
+    sa.sa_handler = SIG_IGN;
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+}
+
 
 int main(int argc, char const *argv[]) {
 
     char command_line[1000];
 
+    printf("Bilgi 1) Cikmak icin exit komutunu kullaniniz.\n");
+    printf("Bilgi 2) Komutlarda pipe isareti kullanilabilir.\n\n");
+
+    /* ctrl-c yapildiginda shell in sonlanmamasi icin int sinyal handleri overwrite edilerek ign (ignore) yapilir */
+    ignore_int_signal();
 
         while (1) {
             printf("17011603-shell$ ");
